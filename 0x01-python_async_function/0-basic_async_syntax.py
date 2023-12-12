77f78c152f8bfb0f8233ffe3ac9d1b4d6aa28a76
davidajimati <ajimatidavid@gmail.com>
2023-12-12 08:37:53 +0100

    added return type to function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-
-import asyncio
-import random
 """
 asynchronous coroutine that takes in an integer argument (max_delay,
 with a default value of 10) named wait_random that waits
@@ -10,8 +7,11 @@ for a random delay between 0and max_delay (included and float value)
 seconds and eventually returns it.
 """
 
+import asyncio
+import random
 
-async def wait_random(max_wait: int = 10):
+
+async def wait_random(max_wait: int = 10) -> float:
     """
     wait_random accepts a variable, generates a random number,
     waits for that long and returns it
